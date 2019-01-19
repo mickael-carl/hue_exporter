7e0d5bdfd08a120d60d7a1e5bf76df533b529c1b
Mickael Carl <mickael.carl@prodrive-technologies.com>
2019-01-19 23:43:28 +0100

    Add some niceties found usually in exporters
    E.g. metrics path, exporter port and a landing page

--- cmd/hue_exporter.go
+++ cmd/hue_exporter.go
@@ -15,9 +15,11 @@ var (
 		prometheus.BuildFQName("hue", "", "scrape_success"),
 		"Whether scraping the Hue devices was successful.",
 		nil, nil)
-	app = kingpin.New("hue_exporter", "A Prometheus exporter for the Philips Hue IoT system.")
+	app           = kingpin.New("hue_exporter", "A Prometheus exporter for the Philips Hue IoT system.")
 	bridgeAddress = app.Flag("bridge", "Bridge HTTP address.").Required().String()
-	userToken = app.Flag("token", "The user token used to authenticate requests against the bridge.").Required().String()
+	userToken     = app.Flag("token", "The user token used to authenticate requests against the bridge.").Required().String()
+	listenAddress = app.Flag("listen-address", "Address on which to expose metrics.").Default(":9535").String()
+	metricsPath   = app.Flag("metrics-path", "Path under which to expose metrics.").Default("/metrics").String()
 )
 
 type HueExporter struct {
@@ -60,7 +62,19 @@ func main() {
 	if err != nil {
 		log.Error(err)
 	}
+	log.Infoln("Starting hue_exporter")
+	log.Infoln("Exporting metrics from Hue bridge located at", exporter.address)
 	prometheus.MustRegister(exporter)
-	http.Handle("/metrics", promhttp.Handler())
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.Handle(*metricsPath, promhttp.Handler())
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`<html>
+			<head><title>Hue Exporter</title></head>
+			<body>
+			<h1>Hue Exporter</h1>
+			<p><a href="` + *metricsPath + `">Metrics</a></p>
+			</body>
+			</html>`))
+	})
+	log.Infoln("Listening on", *listenAddress)
+	log.Fatal(http.ListenAndServe(*listenAddress, nil))
 }
